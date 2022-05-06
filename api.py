e6eb2c7ea5a28cbae4532cc1e0aa2b2846a4c978
DmitShu <dmitshu@gmail.com>
2022-05-06 08:43:44 +0300

    Еще методы

--- api.py
+++ api.py
@@ -105,3 +105,25 @@ class PetFriends:
         except json.decoder.JSONDecodeError:
             result = res.text
         return status, result
+
+    def add_new_pet_simple(self, auth_key: json, name: str, animal_type: str,
+                    age: str) -> json:
+        """Метод отправляет (постит) на сервер данные о добавляемом питомце (без фото) и возвращает статус
+        запроса на сервер и результат в формате JSON с данными добавленного питомца"""
+
+        headers = {'auth_key': auth_key['key']}
+        data = {
+            'name': name,
+            'age': age,
+            'animal_type': animal_type
+        }
+
+        res = requests.post(self.base_url + 'api/create_pet_simple', headers=headers, data=data)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        print(result)
+        return status, result
