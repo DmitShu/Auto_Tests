0f2e450a2815dd5ec048ee0b8cf037e8047b9285
DmitShu <dmitshu@gmail.com>
2022-05-08 10:58:45 +0300

    new tests

---
+++ tests/pet_friends_test_run_4_foreign_pets.py
@@ -0,0 +1,9 @@
+# TestRun 4
+# Блок тестов для описанных методов PetFriends API v1 (https://petfriends1.herokuapp.com/apidocs/#/)
+# Проверяются методы на возможность изменения / удаления данных чужих пользователей.
+
+from api import PetFriends
+from settings import *
+import os
+
+pf = PetFriends()
