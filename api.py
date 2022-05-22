3ad6dc90f55e01c0b85772d1f99a73b31928cf1a
DmitShu <dmitshu@gmail.com>
2022-05-22 11:33:22 +0300

    Tests logging

--- api.py
+++ api.py
@@ -6,6 +6,7 @@ from datetime import datetime
 
 def logrequests(func):
     """Логирует запросы и ответы"""
+
     def wrapper_log(*args, **kwargs):
         args_repr = [repr(a) for a in args]
         kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]
