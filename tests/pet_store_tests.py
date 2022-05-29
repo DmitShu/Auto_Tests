a85f35b2d6b3d7d903898ad4bd4bdcc2288c5e27
DmitShu <dmitshu@gmail.com>
2022-05-29 11:17:06 +0300

    add_new_pet with test

--- tests/pet_store_tests.py
+++ tests/pet_store_tests.py
@@ -72,8 +72,26 @@ def test_get_findByStatus_negative(filter):
    assert 'id' and 'category' and 'name' and 'photoUrls' not in str(result)
    assert pytest.status == 200
 
-
-def test_add_new_pet_store(name = "Тест олень", status ='available',
+@pytest.mark.parametrize("name", [
+   ''
+   , generate_string(255)
+   , generate_string(1001)
+   , russian_chars()
+   , russian_chars().upper()
+   , chinese_chars()
+   , special_chars()
+   , '123'
+], ids=[
+   'empty'
+   , '255 symbols'
+   , 'more than 1000 symbols'
+   , 'russian'
+   , 'RUSSIAN'
+   , 'chinese'
+   , 'specials'
+   , 'digit'
+])
+def test_add_new_pet_store(name, status ='available',
                     name_category = '', name_tag = ''):
     """Проверяем что можно добавить питомца без фото с корректными данными"""
 
@@ -82,3 +100,22 @@ def test_add_new_pet_store(name = "Тест олень", status ='available',
 
     # Сверяем полученный ответ с ожидаемым результатом
     assert status == 200
+    assert result['name'] == name
+    assert result['status'] == status
+
+# def is_age_valid(age):
+#    # Проверяем, что возраст - это число от 1 до 49 и целое
+#    return age.isdigit() \
+#           and 0 < int(age) < 50 \
+#           and float(age) == int(age)
+#
+#
+# @pytest.mark.parametrize("name"
+#    , ['', generate_string(255), generate_string(1001), russian_chars(), russian_chars().upper(), chinese_chars(), special_chars(), '123']
+#    , ids=['empty', '255 symbols', 'more than 1000 symbols', 'russian', 'RUSSIAN', 'chinese', 'specials', 'digit'])
+# @pytest.mark.parametrize("animal_type"
+#    , ['', generate_string(255), generate_string(1001), russian_chars(), russian_chars().upper(), chinese_chars(), special_chars(), '123']
+#    , ids=['empty', '255 symbols', 'more than 1000 symbols', 'russian', 'RUSSIAN', 'chinese', 'specials', 'digit'])
+# @pytest.mark.parametrize("age"
+#    , ['', '-1', '0', '1', '100', '1.5', '2147483647', '2147483648', special_chars(), russian_chars(), russian_chars().upper(), chinese_chars()]
+#    , ids=['empty', 'negative', 'zero', 'min', 'greater than max', 'float', 'int_max', 'int_max + 1', 'specials', 'russian', 'RUSSIAN', 'chinese'])
